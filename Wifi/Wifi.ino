4d99834c30d62a30abf1ab066f0fd41a06850c6d
paulo-ricardo <paulo_r_joao@estudante.sesisenai.org.br>
2025-10-01 17:17:04 -0300

    reconectar a rede wifi

--- Wifi/Wifi.ino
+++ Wifi/Wifi.ino
@@ -1,39 +1,46 @@
 #include <WiFi.h>
 
-const String SSID = "wifi";
-const String PSWD = "senha";
+const String SSID = "Paulo";
+const String PSWD = "cxos9674";
 
-
-void scanLocalNetworks();
+void connectToWiFi();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(1152200);
-  scanLocalNetworks();
-  Serial.println("Iniciando conexão com rede WiFi");
-  WiFi.begin(SSID,PSWD);
-  while(WiFi.status() != WL_CONNECTED){
-    Serial.print(".");
-    delay(200);
-  }
-  Serial.println("\nConectado");
+  connectToWiFi();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-void scanLocalNetworks(){
-  Serial.println("Iniciando Scan de redes Wi-Fi");
-
-  int number = WiFi.scanNetworks();
-  delay(500);
-
-  if(number == -1){
-    Serial.println("ERRO!. Não deu bom");
-  }else{
-    Serial.printf("Número de reders encontradas: %d\n", number);
-    for(int net = 0; net < number; net++){
-      Serial.printf("%d - %s | %d db\n", net, WiFi.SSID(net), WiFi.RSSI(net));
-    }
+  if(WiFi.status() != WL_CONNECTED){
+    Serial.println("conexão perdida");
+    connectToWiFi();
   }
 }
+
+void connectToWiFi(){
+  Serial.println("Iniciando conexão con rede wifi");
+  WiFi.begin(SSID,PSWD);
+    while(WiFi.status() != WL_CONNECTED){
+      Serial.print(".");
+      delay(200);
+    }
+    
+}
+
+// void scanLocalNetworks(){
+//   Serial.println("Iniciando Scan de redes Wi-Fi");
+
+//   int number = WiFi.scanNetworks();
+//   delay(500);
+
+//   if(number == -1){
+//     Serial.println("ERRO!. Não deu bom");
+//   }else{
+//     Serial.printf("Número de reders encontradas: %d\n", number);
+//     for(int net = 0; net < number; net++){
+//       Serial.printf("%d - %s | %d db\n", net, WiFi.SSID(net), WiFi.RSSI(net));
+//     }
+//   }
+// }
