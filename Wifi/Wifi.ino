601367f0a522dd66f8395a26a66dea448c6de0bc
paulo-ricardo <paulo_r_joao@estudante.sesisenai.org.br>
2025-10-01 16:13:59 -0300

    implementada conexão simples com wifi

--- Wifi/Wifi.ino
+++ Wifi/Wifi.ino
@@ -1,28 +1,39 @@
 #include <WiFi.h>
 
+const String SSID = "Paulo";
+const String PSWD = "cxos9674";
+
+
 void scanLocalNetworks();
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(1152200);
-  Serial.println("Iniciando Scan de redes Wi-Fi");
-
+  scanLocalNetworks();
+ ; Serial.println("Iniciando conexão com rede WiFi");
+  WiFi.begin(SSID,PSWD);
+  while(WiFi.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-
+}
 void scanLocalNetworks(){
+  Serial.println("Iniciando Scan de redes Wi-Fi");
+
   int number = WiFi.scanNetworks();
   delay(500);
+
   if(number == -1){
     Serial.println("ERRO!. Não deu bom");
   }else{
     Serial.printf("Número de reders encontradas: %d\n", number);
     for(int net = 0; net < number; net++){
-      Serial.printf("%d - %s | %d db\n", net, WiFi.SSID(number), WiFi.RSSI(net));
+      Serial.printf("%d - %s | %d db\n", net, WiFi.SSID(net), WiFi.RSSI(net));
     }
   }
-}
 }
