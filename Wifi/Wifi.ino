5ace15103da94b85e5927f720f1cab0edee8980b
paulo-ricardo <paulo_r_joao@estudante.sesisenai.org.br>
2025-10-01 15:37:37 -0300

    Criada função para escanear redes

--- Wifi/Wifi.ino
+++ Wifi/Wifi.ino
@@ -1,22 +1,28 @@
 #include <WiFi.h>
 
+void scanLocalNetworks();
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(1152200);
-  Serial.printIn("Iniciando Scan de redes Wi-Fi");
+  Serial.println("Iniciando Scan de redes Wi-Fi");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int number = WiFi.scanNetWorks();
+
+
+void scanLocalNetworks(){
+  int number = WiFi.scanNetworks();
   delay(500);
   if(number == -1){
-    Serial.printIn("ERRO!. Não deu bom")
+    Serial.println("ERRO!. Não deu bom");
   }else{
     Serial.printf("Número de reders encontradas: %d\n", number);
     for(int net = 0; net < number; net++){
       Serial.printf("%d - %s | %d db\n", net, WiFi.SSID(number), WiFi.RSSI(net));
     }
   }
+}
 }
